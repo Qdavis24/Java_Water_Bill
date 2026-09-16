5cbed964c9cca0d2c7f8e6c4777e9f467a668ef0
Quinn Davis <Quoncdavis@gmail.com>
2026-09-16 14:44:42 -0600

    add unit test for bill

--- src/Main.java
+++ src/Main.java
@@ -4,26 +4,8 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args){
-
         Customer cmer = new Customer();
-
-
-        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
-        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-        try {
-            System.out.print("Enter Customer Name: ");
-            name = bufferedReader.readLine();
-            System.out.print("Enter Customer Type (1: SingleFamily, 2: Duplex): ");
-            customerType = Integer.parseInt(bufferedReader.readLine());
-            System.out.print("Enter gallons used: ");
-            gallonsUsed = Integer.parseInt(bufferedReader.readLine());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-
-        System.out.println("the bill is " + bill);
-
+        cmer.printBill();
     }
 
 }
